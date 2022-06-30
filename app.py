f27efc2e05c17f76828970742f3bcede2beac84d
xRA1Nx <stwski@gmail.com>
2022-06-30 21:45:48 +0300

    work done

--- app.py
+++ app.py
@@ -1,22 +1,64 @@
+#!/usr/bin/env python
+
 import telebot
 import os
+
 from pathlib import Path
 from dotenv import load_dotenv
+from config import d_massages
+
+from db_and_pars_functions import start_parser, get_cities, get_city_info
 
 env_path = Path('.') / '.env'
 load_dotenv(dotenv_path=env_path)
-
 TOKEN = os.getenv("TOKEN")
 
 
-bot = telebot.TeleBot(TOKEN)
+def app_start():
+    bot = telebot.TeleBot(TOKEN)
+    bot_name = bot.get_me().username  # получаем имя нашего бота
+
+    @bot.message_handler(commands=["start"])
+    def bot_info(message: telebot.types.Message):
+        bot.reply_to(message, d_massages["start"])
+
+    @bot.message_handler(commands=["parse"])
+    def bot_upd_db(message: telebot.types.Message):
+        start_parser()
+        bot.reply_to(message, d_massages['parse'])
+
+    @bot.message_handler(commands=["cities"])
+    def bot_get_cities(message: telebot.types.Message):
+        city_list = get_cities()
+        city_str = "".join(list(map(lambda x: x + "\n", city_list)))
+        bot.reply_to(message, city_str)
+
+    @bot.message_handler(content_types=["text"])
+    def get_city_details(message: telebot.types.Message):
+        res_text = message.text.title()
+
+        if len(message.text) <= 40:  # cкипаем все сообщения длиной более 40 символов
+            if f"@{bot_name}" in message.text.split():  # если тагнут бот
+                message.text = "".join(list(filter(lambda x: x != f"@{bot_name}", message.text.split())))
+                relevant_cities_list = list(
+                    filter(lambda x: message.text.lower() in x, map(lambda s: s.lower(), get_cities())))
+                relevant_cities_msg = "".join(list(map(lambda x: x.title() + "\n", relevant_cities_list)))
+
+                # если есть города содержащие введеный текст
+                if relevant_cities_msg:
+                    bot.send_message(message.chat.id,
+                                     f"список подходящих для '{message.text}' городов:\n" + relevant_cities_msg)
+                else:
+                    bot.send_message(message.chat.id,
+                                     f"нет подходящих городов для '{message.text}'" + relevant_cities_msg)
+
+            # Если не тагаем бота, а пишим просто в чат
+            elif res_text in get_cities():
+                bot.send_message(message.chat.id, f"население города {res_text}: " + get_city_info(res_text))
+
+    bot.polling(none_stop=True)
 
 
-# @bot.message_handler(content_types="text")
-@bot.message_handler(commands=["start", "help"])
-def info(message: telebot.types.Message):
-    bot.reply_to(message, "привет")
-    # bot.send_message(message.chat.id, f"Welcome, {message.chat.username}")
-
-
-bot.polling(none_stop=True)
+if __name__ == "__main__":
+    start_parser() # создаем бд и наполняем/обновляем
+    app_start() # cтартуем приложение
---
+++ db_and_pars_functions.py
@@ -0,0 +1,93 @@
+import requests
+import os
+import psycopg2
+
+from bs4 import BeautifulSoup
+from config import *
+from pathlib import Path
+from dotenv import load_dotenv
+
+env_path = Path('.') / '.env'
+load_dotenv(dotenv_path=env_path)
+
+
+def start_connection() -> tuple:
+    conn = psycopg2.connect(f"dbname={os.getenv('DB')} user={os.getenv('USER')} password={os.getenv('PASSWORD')}")
+    cur = conn.cursor()
+    return conn, cur
+
+
+def get_cities() -> list:
+    conn, cur = start_connection()
+    cur.execute("SELECT city from cityinfo")
+    query_list = cur.fetchall()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return list(map(lambda x: str(x[0]).title(), query_list))
+
+
+def get_city_info(city) -> str:
+    conn, cur = start_connection()
+    cur.execute(f"SELECT population from cityinfo WHERE city = '{city}'")
+    item = cur.fetchone()
+    conn.commit()
+    cur.close()
+    conn.close()
+    return str(item[0])
+
+
+def start_parser() -> None:
+    # Подключаемся к БД и создаем таблицу(если она еще не была создана)
+    conn, cur = start_connection()
+    cur.execute("""CREATE TABLE IF NOT EXISTS cityinfo (
+        id serial PRIMARY KEY, 
+        city varchar(50), 
+        population INTEGER, 
+        link VARCHAR
+        );""")
+
+    # делаем запро по всем горадам в базе данных
+    cur.execute("SELECT * from cityinfo")
+    query_list = cur.fetchall()
+    # переводим список множест в множество городов для дальнейшего быстрого поиска
+    cities_query_list = set(map(lambda x: x[1], query_list))
+    check_for_upd_dict = {}  # cловарь который будем использовать при проверки на необходимость UPD записи в БД
+    for row in query_list:
+        check_for_upd_dict[row[1]] = str(row[2]) + row[3]
+
+    r = requests.get(url).content
+    soup = BeautifulSoup(r, 'lxml')
+    table = soup.find("table", class_="standard sortable")
+    tbody = table.find("tbody")
+    trs = tbody.find_all("tr")
+
+    # формируем словарь с номерами полей, на случай если столбцы в таблици в wiki поменяется
+    field_name_dict = {}
+    for ind, item in enumerate(trs[0]):
+        field_name_dict[item.getText()] = ind
+    pop_ind = field_name_dict["население,чел."]  # индекс поля с населением
+    city_ind = field_name_dict["город"]  # индекс поля с названием города
+
+    for tr in trs[1:]:
+        tds = tr.find_all("td")
+        population = tds[pop_ind].get('data-sort-value')
+        city = tds[city_ind].getText().title()
+        link = root_wiki_url + tds[city_ind].a.get("href")
+
+        if city not in cities_query_list:  # если нет в БД записи с таким городом, то делаем INSERT
+            cur.execute(f"""
+                INSERT INTO cityinfo (city, population, link)
+                VALUES ('{city}', {population}, '{link}')
+                """)
+        # если спарсенные данные ссылки+популяция не совпадают, то обновляем эту строку в БД
+        elif str(population) + link != check_for_upd_dict[city]:
+            cur.execute(f"""
+                UPDATE cityinfo 
+                SET population = {population}, link = '{link}' 
+                WHERE city = '{city}'
+                """)
+
+    conn.commit()
+    cur.close()
+    conn.close()
